7454affcdbde7ca51a84ad79c1b077aa11c52591
OscarPALISSOT <oscar.p25000@gmail.com>
2025-01-16 00:40:48 +0100

    searchbar

---
+++ app/src/components/searchBar.tsx
@@ -0,0 +1,21 @@
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+interface SearchBarProps {
+    placeholder: string;
+}
+
+const SearchBar = ({placeholder}: SearchBarProps) => {
+    return (
+        <div className={'px-2 py-1 rounded-md bg-primary hover:bg-primaryHover font-light flex items-center justify-center gap-2 focus-within:outline outline-blue-500'}>
+            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+            <input
+                type={'text'}
+                className={'bg-transparent focus:outline-none'}
+                placeholder={placeholder}
+            />
+        </div>
+    )
+}
+
+export default SearchBar;
