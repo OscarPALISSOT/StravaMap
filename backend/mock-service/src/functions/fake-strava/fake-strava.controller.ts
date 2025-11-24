bcef80a50eaae1469f4209184ac38dbaa2e561fa
OscarPALISSOT <oscar.p25000@gmail.com>
2025-11-25 00:26:29 +0100

    strava oauth mock

--- backend/mock-service/src/functions/fake-strava/fake-strava.controller.ts
+++ backend/mock-service/src/functions/fake-strava/fake-strava.controller.ts
@@ -1,5 +1,6 @@
-import { BadRequestException, Controller, Get, Query, Res } from "@nestjs/common";
+import { BadRequestException, Controller, Get, Post, Query, Res } from "@nestjs/common";
 import type { Response } from 'express';
+import { DateTime } from 'luxon';
 
 @Controller('fake-strava')
 export class FakeStravaController {
@@ -13,8 +14,8 @@ export class FakeStravaController {
     @Res() res: Response,
   ) {
 
-    if (!redirectUri) {
-      throw new BadRequestException('redirect_uri is required');
+    if (!redirectUri || !scope || !clientId) {
+      throw new BadRequestException('missing parameter');
     }
     if (responseType !== 'code') {
       return res.status(400).send('response_type must be "code"');
@@ -25,4 +26,50 @@ export class FakeStravaController {
 
     return res.redirect(url);
   }
+
+  @Post('oauth/token')
+  token(
+    @Query('client_id') clientId: string,
+    @Query('client_secret') client_secret: string,
+    @Query('code') code: string,
+    @Query('grant_type') grant_type: string,
+    @Res() res: Response,
+  ) {
+    if (!clientId || !client_secret || !code || !grant_type) {
+      throw new BadRequestException('missing parameter');
+    }
+    if (grant_type !== 'authorization_code') {
+      return res.status(400).send('grant_type must be "authorization_code"');
+    }
+    return res.json({
+      token_type: "Bearer",
+      expires_at: DateTime.now().plus({hours: 6}).toUnixInteger(),
+      expires_in: 21600,
+      refresh_token: "f7f3a69f8846fb2c0b1756f66G8650ffd6b976f2",
+      access_token: "43844e53bd9f3f7e4c18543b9d899e84a3a79269",
+      athlete: {
+        id: 116651093,
+        username: null,
+        resource_state: 2,
+        firstname: "John",
+        lastname: "Doe",
+        bio: "Some bio",
+        city: "Grenoble",
+        state: "Isère",
+        country: "France",
+        sex: "M",
+        premium: false,
+        summit: false,
+        created_at: "2023-04-18T13:34:42Z",
+        updated_at: "2025-11-23T23:12:06Z",
+        badge_type_id: 0,
+        weight: 92.0,
+        profile_medium: "https://picsum.photos/60",
+        profile: "https://picsum.photos/124",
+        friend: null,
+        follower: null
+      }
+    });
+  }
+
 }
